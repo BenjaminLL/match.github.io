c2e77456810219d482982d3a5511d7ae951a8103
BenjaminLL <szyllll8@gmail.com>
2018-05-17 15:13:25 -0400

    add link to the home

--- assets/js/link.js
+++ assets/js/link.js
@@ -1,4 +1,8 @@
 // link to the SignUp page
 $("#signup").on("click", function() {
-	window.location = "signUp/index.html";
+	window.location = "signUp.html";
+})
+
+$("#home_1, #home_2").on("click", function() {
+	window.location = "index.html";
 })
